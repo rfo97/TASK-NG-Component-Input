889d8bbf12603c0681a42f74f4da8127179c94fb
rfo97 <rfa.alobaid@gmail.com>
2025-04-06 12:13:12 +0300

    added changes to code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { pets } from '../data/pets';
+import { PetComponent } from './pet/pet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ PetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
